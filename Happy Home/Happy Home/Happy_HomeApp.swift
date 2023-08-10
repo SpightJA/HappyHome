2553bb90e48ccd610e1245c279fb84a797c1e8c8
Jonathan <jaspight@hotmail.com>
2023-08-10 17:21:38 -0500

    intital commit

---
+++ Happy Home/Happy Home/Happy_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Happy_HomeApp.swift
+//  Happy Home
+//
+//  Created by Jon Spight on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Happy_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
